dbdd5f74a5d9a75b1d9549817aabadf2b86c80a2
luxuia <luxuia@foxmail.com>
2016-09-27 00:46:03 +0800

    add input

---
+++ JustEngine/Src/Input.cpp
@@ -0,0 +1,39 @@
+#include "Input.h"
+#include "GameCore.h"
+
+#include <XInput.h>
+#pragma  comment(lib, "xinput_9_1_0.lib")
+
+#include <dinput.h>
+#pragma  comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+namespace GameCore
+{
+	extern HWND g_hWnd;
+}
+
+namespace JustEngine
+{
+	#define NumInputs  static_cast<Input::KEY_TYPE>(InputKey::NumInputs)
+	bool sButton[ 2 ][ NumInputs ];
+	float sHoldDuration[ NumInputs ];
+
+	IDirectInput8A* sDI;
+	IDirectInputDevice8A* sKeyboard;
+	IDirectInputDevice8A* sMouse;
+
+	_DIMOUSESTATE2 sMouseState;
+	unsigned char sKeybuffer[ 256 ];
+	unsigned char sDXKeyMapping[ static_cast<Input::KEY_TYPE>(InputKey::NumKeys) ];
+
+	bool Input::IsAnyPressed()
+	{
+	
+	}
+
+	bool Input::IsPressed(InputKey key)
+	{
+	
+	}
+}
